37894315522e9483206e5bcfa37b63e2a6d5a3cc
OnlySoupy <dylaneddie@gmail.com>
2024-05-03 19:20:33 +0100

    added cart route and annotation

--- agile-art/routes/web.php
+++ agile-art/routes/web.php
@@ -4,12 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+// Route for dashboard
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified']) // Requirement - user must be authenticated AND verified to access the cart.
     ->name('dashboard');
 
+// Route for Profile
 Route::view('profile', 'profile')
-    ->middleware(['auth'])
+    ->middleware(['auth']) // Requirement - user must be authenticated to access the cart.
     ->name('profile');
+
+// Route for cart
+Route::view('cart', 'cart')
+    ->middleware(['auth']) // Requirement - user must be authenticated to access the cart.
+    ->name('cart');
 
 require __DIR__.'/auth.php';
