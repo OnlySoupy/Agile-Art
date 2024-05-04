48a8d18f670f68086cccc7db35c83ee114b12b49
OnlySoupy <dylaneddie@gmail.com>
2024-05-04 11:38:19 +0100

    added OrderItem and Order models

---
+++ agile-art/app/Livewire/Components/ProductContainer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+
+class ProductContainer extends Component
+{
+    public function render()
+    {
+        return view('livewire.product-container');
+    }
+}
